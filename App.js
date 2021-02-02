36b59f17a097d71e8b060b8b356e10daa857f8a0
gwanggyu park <pgg961@gmail.com>
2021-02-02 16:01:45 +0900

    get permission in react-native

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Loading from "./Loading";
 
-export default function App() {
-  return <Loading/>;
+import React from 'react';
+import Loading from "./Loading";
+import {Alert} from "react-native";
+import * as Location from "expo-location";
+
+export default class extends React.Component{
+  // method get location using expo-location in async
+  getLocation = async() => {
+    try {
+      const response = await Location.getPermissionsAsync();
+      console.log(responese);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad. ");
+    }
+    
+  }
+
+  // get location using expo-location
+  componentDidMount(){
+    this.getLocation();
+  }
+  
+  // when use class, render should be called
+  render(){
+    return <Loading/>;
+  }
 }
 
